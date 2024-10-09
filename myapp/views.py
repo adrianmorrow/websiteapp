b40fb646c7bb2cf141b544461bfff39a2aa510e0
adrianmorrow <adrian@2morrowz.com>
2024-10-09 07:06:06 -0700

    Added images to gallery and contact form

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from myapp.forms import ContactForm
+from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
 def home(request):
@@ -12,3 +14,17 @@ def contact(request):
 
 def gallery(request):
         return render(request, 'gallery.html')
+
+
+def contact(request):
+        if request.method == 'POST':
+                form = ContactForm(request.POST)
+                if form.is_valid():
+                        # Process the form data
+                        return redirect('success')
+        else:
+                form = ContactForm()
+        return render(request, 'contact.html', {'form': form})
+
+def success(request):
+        return HttpResponse('Success!')
